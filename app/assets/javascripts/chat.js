349dab71125a7435f1a5b26ae98e5faf16a6d7b4
asmcos <asmcos@gmail.com>
2013-04-18 00:16:02 +0800

    liveroom show

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -9,8 +9,7 @@
     
     
     liveterm = new Terminal(80, 24);
-    if (liveterm)
-    	liveterm.open("liveterm1");
+    liveterm.open("liveterm1");
 
     url = "ws://127.0.0.1:8088";
     ws = new WebSocket(url);
